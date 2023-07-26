22fbc0ecb15c8cbb76440a98ce5c78fa7d861fb3
Shyju-Mathews <91419372+Shyju-Mathews@users.noreply.github.com>
2023-07-26 10:42:09 +0530

    fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = ({title}) => {
   const { width } = useContext(DataContext)
   return (
     <header className="header">
-      <h1>{title}</h1>
+      <h1>{title}</h1> 
       {
         width < 768 ? <FaMobileAlt /> : width < 992 ? <FaTabletAlt /> : <FaLaptop />
       }
